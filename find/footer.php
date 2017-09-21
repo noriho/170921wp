faa84e1ad3bbe2589bf5035761d4e77287f114ad
hotta_noriyuki <hotta_noriyuki@DESKTOP-4FDN1UT>
2017-09-21 14:44:25 +0900

    commit

---
+++ find/footer.php
@@ -0,0 +1,16 @@
+<footer>
+<div class="container">
+	<small>
+		Copyright &copy; <?php bloginfo( 'name' ); ?>
+	</small>
+</div>
+
+<?php dynamic_sidebar('footerbar');  ?>
+<?php dynamic_sidebar('footer_left');  ?>
+<?php dynamic_sidebar('footer_right');  ?>
+</footer>
+
+
+<?php wp_footer(); ?> 
+</body>
+</html>
